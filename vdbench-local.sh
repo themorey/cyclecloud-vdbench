ab8418aae42bdc078423747a182534349d27b522
Jerry Morey <24438151+themorey@users.noreply.github.com>
2021-03-08 15:14:44 -0500

    Add files via upload

---
+++ vdbench-local.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# arg: $1 = NFS mountpoint to test (ie. /avere )
+testmount=$1
+
+FILENAME="$testmount/vdbench/azure-clients.conf"
+
+for vm in $FILENAME; do
+    ssh $vm "sudo yum install -y openjdk; chmod -R /mnt/resource"
+done
+
+# Create the associative array that contains the job name and threads per host values
+declare -A threadsperhost
+threadsperhost=( ["00_fsd1_format"]=6 ["00_fsd2_format"]=6 ["01_large_10_90_throughput"]=6 ["02_large_50_50_throughput"]=6 ["03_large_90_10_throughput"]=6 ["04_max_read_iops"]=64 ["05_max_read_throughput"]=6 ["06_max_write_iops"]=64 ["07_max_write_throughput"]=6 ["08_small_10_90_throughput"]=64 ["09_small_50_50_throughput"]=64 ["10_small_90_10_throughput"]=64  )
+ 
+# Create a declarative array to index the jobs so they run in order
+declare -a orderjobs
+orderjobs=( "00_fsd1_format" "00_fsd2_format" "01_large_10_90_throughput" "02_large_50_50_throughput" "03_large_90_10_throughput" "04_max_read_iops" "05_max_read_throughput" "06_max_write_iops" "07_max_write_throughput" "08_small_10_90_throughput" "09_small_50_50_throughput" "10_small_90_10_throughput" )
+
+#move to /shared/apps/vdbench as fsd files are relative to that path
+cd /shared/apps/vdbench
+
+# Loop through the array(s) to run the vdbench jobs
+for K in "${orderjobs[@]}"; do
+    /shared/apps/vdbench/vdbench -f /shared/apps/vdbench/benchmarks/vdbench/filesys/$K includeFile=$FILENAME lun=$testmount/vdbench threadsPerHost=${threadsperhost[$K]} -o /mnt/resource/job_${K}_node_$(jetpack config hostname)_$(date +"%Y%m%d_%I%M%p").out
+    wait
+done
+
+cp /mnt/resource/{*.out,*.err} $testmount/vdbench-output/
+rm -rf $testmount/vdbench/fsd*
---
+++ vdbench-sbatch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=nfs-vdbench
+##SBATCH --partition=default
+#SBATCH -N 24
+##SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+##SBATCH -t 04:00:00 # time required, here it is 4 hours
+
+# arg: $1 = NFS mountpoint to test (ie. /avere )
+testmount=$1
+
+FILENAME="$testmount/vdbench/azure-clients.conf"
+rm -f $FILENAME
+
+if [ -d "$testmount/vdbench" ]; then
+    echo "$testmount/vdbench exists"
+else
+    mkdir $testmount/vdbench
+    mkdir $testmount/vdbench-output
+    chmod 777 $testmount/vdbench*
+fi
+
+NODE_PREFIX="host"
+/bin/cat <<EOM >$FILENAME
+create_anchors=yes
+hd=default,user=${USER},shell=ssh
+EOM
+
+scontrol show hostnames $SLURM_JOB_NODELIST > tmp-nodefile-$SLURM_JOB_ID
+while IFS= read -r line
+do
+  scontrol show node "$line" | grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" >> nodefile-$SLURM_JOB_ID-tmp
+done < "tmp-nodefile-$SLURM_JOB_ID"
+uniq nodefile-$SLURM_JOB_ID-tmp nodefile
+rm nodefile-$SLURM_JOB_ID-tmp tmp-nodefile-$SLURM_JOB_ID
+
+echo "cat nodefile:"
+cat nodefile
+
+COUNTER=0
+uniq nodefile |
+{
+while read i; do
+    HOST_NUMBER=$(($COUNTER + 1))
+    HOST_NUMBER_HEX=$( printf '%02x' $HOST_NUMBER )
+    NODE_NAME="${NODE_PREFIX}-${COUNTER}"
+    ###IP=$( getent hosts ${i} |  awk '{print $1}' )
+    echo "NODE NAME ${NODE_NAME}, ${i}"
+    echo "hd=host${HOST_NUMBER_HEX},system=${i}" >> $FILENAME
+    COUNTER=$[$COUNTER+1]
+done
+}
+
+batchHost=$(scontrol show job $SLURM_JOB_ID | grep BatchHost | cut -d\= -f2)
+srun -N 1 -w $batchHost /shared/apps/vdbench/vdbench-local.sh $testmount
